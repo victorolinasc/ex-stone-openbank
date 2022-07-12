b6cca82ec7af4d9a3af9ac5deca7ce3abaf8380f
Caroline Noronha <carolnte@gmail.com>
2022-07-12 11:33:18 -0300

    chore: adjust specs

--- lib/ex_stone_openbank/api/payments.ex
+++ lib/ex_stone_openbank/api/payments.ex
@@ -15,8 +15,6 @@ defmodule ExStoneOpenbank.API.Payments do
 
   This is needed in order to understand the details of the payment and show it up front to users.
   """
-  @spec dry_run_pay_barcode(config_name :: atom(), input :: map()) ::
-          {:ok, BarcodePaymentResponse.t()} | {:error, reason :: atom()}
   def dry_run_pay_barcode(config_name, input) do
     case DryRunBarcodePaymentInput.cast_and_apply(input) do
       %DryRunBarcodePaymentInput{} = input ->
@@ -31,8 +29,6 @@ defmodule ExStoneOpenbank.API.Payments do
   @doc """
   Pay a barcode.
   """
-  @spec pay_barcode(config_name :: atom(), input :: map()) ::
-          {:ok, BarcodePaymentResponse.t()} | {:error, reason :: atom()}
   def pay_barcode(config_name, input) do
     case BarcodePaymentInput.cast_and_apply(input) do
       %BarcodePaymentInput{} = input ->
